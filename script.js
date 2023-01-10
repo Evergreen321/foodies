aa8d9b29be94b5db537ca83b7445a48a6c521216
Yuliia <yolochka321@gmail.com>
2023-01-10 14:46:16 -0500

    Fix burger menu and clean code

--- script.js
+++ script.js
@@ -6,89 +6,10 @@ const currentYear = new Date().getFullYear();
 
 copyright.textContent = `Copyright \u00A9 ${currentYear} Foodies.`;
 
-// DEFINE THE DEVICE
-const isMobile = {
-  Android: function () {
-    return navigator.userAgent.match(/Android/i);
-  },
-  BlackBerry: function () {
-    return navigator.userAgent.match(/BlackBerry/i);
-  },
-  iOS: function () {
-    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-  },
-  Opera: function () {
-    return navigator.userAgent.match(/Opera Mini/i);
-  },
-  Windows: function () {
-    return navigator.userAgent.match(/IEMobile/i);
-  },
-  any: function () {
-    return (
-      isMobile.Android() ||
-      isMobile.BlackBerry() ||
-      isMobile.iOS() ||
-      isMobile.Opera() ||
-      isMobile.Windows()
-    );
-  },
-};
-
-if (isMobile.any()) {
-  document.body.classList.add("_touch");
-
-  let menuArrows = document.querySelectorAll(".menu__arrow");
-  if (menuArrows.length > 0) {
-    for (let index = 0; index < menuArrows.length; index++) {
-      const menuArrow = menuArrows[index];
-      menuArrow.addEventListener("click", function (e) {
-        menuArrow.parentElement.classList.toggle("_active");
-      });
-    }
-  }
-} else {
-  document.body.classList.add("_pc");
-}
-
-// Scrolling after click
-const menuLinks = document.querySelectorAll(".menu__link[data-goto]");
-
-if (menuLinks.length > 0) {
-  menuLinks.forEach((menuLink) => {
-    menuLink.addEventListener("click", onMenuLinkClick);
-  });
-
-  function onMenuLinkClick(e) {
-    const menuLink = e.target;
-    // проверка есть ли действительно такой объект в html
-    if (
-      menuLink.dataset.goto &&
-      document.querySelector(menuLink.dataset.goto)
-    ) {
-      const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      const gotoBlockValue =
-        gotoBlock.getBoundingClientRect().top +
-        pageYOffset -
-        document.querySelector("header").offsetHeight;
-
-      if (iconMenu.classList.contains("_active")) {
-        document.body.classList.remove("_lock");
-        iconMenu.classList.remove("_active");
-        menuBody.classList.remove("_active");
-      }
-
-      window.scrollTo({
-        top: gotoBlockValue,
-        behavior: "smooth",
-      });
-      e.preventDefault();
-    }
-  }
-}
-
 // HAMBURGER MENU
 const menu = document.querySelector(".main-nav-list");
 const menuItems = document.querySelectorAll(".main-nav-link");
+const headerEl = document.querySelector(".header");
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".closeIcon");
 const menuIcon = document.querySelector(".menuIcon");
@@ -98,33 +19,19 @@ function toggleMenu() {
     menu.classList.remove("showMenu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
+    headerEl.classList.toggle("nav-open");
   } else {
     menu.classList.add("showMenu");
     closeIcon.style.display = "block";
     menuIcon.style.display = "none";
+    headerEl.classList.toggle("nav-open");
   }
 }
 
 menuItems.forEach(function (menuItem) {
   menuItem.addEventListener("click", toggleMenu);
 });
-
 hamburger.addEventListener("click", toggleMenu);
-
-// Make mobile navigation work
-
-const btnNavEl = document.querySelector(".hamburger");
-const headerEl = document.querySelector(".header");
-
-btnNavEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
-});
-
-const moves = [10, 20, 30, 40];
-console.log(moves.includes(50));
-const anyMove = moves.some((mov) => mov > 50);
-console.log(anyMove);
-console.log(moves.every((mov) => mov > 0));
 
 // EMAIL INPUT CLEANUP
 const emailInput = document.querySelector(".email-input");
